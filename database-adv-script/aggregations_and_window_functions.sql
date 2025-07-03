b6fd7ef7eb04d9be6123be31de27e90f01199d5c
Frank-003 <oyiengofrankline49@gmail.com>
2025-07-03 10:35:00 +0300

    task 2

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,16 @@
+ELECT 
+    user_id,
+    COUNT(*) AS total_bookings
+FROM 
+    bookings
+GROUP BY 
+    user_id;
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM 
+    bookings
+GROUP BY 
+    property_id;
+
