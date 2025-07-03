83d056264617009cd2ed0ae96fadb20694c5f7a5
Frank-003 <oyiengofrankline49@gmail.com>
2025-07-03 10:27:09 +0300

    task 1

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,30 @@
+SELECT 
+    id, name
+FROM 
+    properties
+WHERE 
+    id IN (
+	        SELECT 
+		            property_id
+			        FROM 
+				            reviews
+					        GROUP BY 
+						            property_id
+							        HAVING 
+								            AVG(rating) > 4.0
+									    );
+
+SELECT 
+    u.id, u.name
+FROM 
+    users u
+WHERE 
+    (
+	        SELECT 
+		            COUNT(*) 
+			        FROM 
+				            bookings b
+					        WHERE 
+						            b.user_id = u.id
+							    ) > 3;
+
